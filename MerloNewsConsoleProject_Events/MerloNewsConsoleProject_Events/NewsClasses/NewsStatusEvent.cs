0cefd5c6321f59026ab0683de9aafb3e3bf92018
RDMerlo <68595137+RDMerlo@users.noreply.github.com>
2021-05-10 04:53:21 +0500

    Паттерн Наблюдение через события

---
+++ MerloNewsConsoleProject_Events/MerloNewsConsoleProject_Events/NewsClasses/NewsStatusEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace MerloNewsConsoleProject_Event.NewsClasses
+{
+    public class NewsStatusEvent: IDisposable
+    {
+        /// <summary>
+        /// Наши события
+        /// </summary>
+        public EventHandler<string> NewsArticle;
+
+        private readonly Timer _timer;
+
+        private readonly MerloPublisher _merloPublisher; //отслеживание новостей
+
+        private void CheckNewNews(Object sourse, ElapsedEventArgs e)
+        {
+            foreach (var news in _merloPublisher.NewNews()) //DeletedFiles
+            {
+                var handler = NewsArticle;
+                handler?.Invoke(this, news);
+            }
+        }
+
+        public NewsStatusEvent(string filename)
+        {
+            _merloPublisher = new MerloPublisher(filename);
+
+            //StartMonitoring
+            if (_merloPublisher.OpenNewsDocument())
+            {
+                _timer = new Timer(1000);
+                _timer.Elapsed += CheckNewNews;
+                _timer.Start();
+            }
+            else
+            {
+                Console.WriteLine("Sorry. Error.");
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
+    }
+}
---
+++ MerloNewsConsoleProject_Events/MerloNewsConsoleProject_Events/Program.cs
@@ -0,0 +1,28 @@
+﻿using MerloNewsConsoleProject_Event.NewsClasses;
+using System;
+
+namespace MerloNewsConsoleProject_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "F://vevit//OneDrive//Study//Practics//Coursework//DesignPattern//MerloNews//NewsFiles//NewsFile.xml";
+
+            var eventObserver = new NewsStatusEvent(path);
+
+            var subscriber1 = new MerloSubscriber("Petrov");
+            var subscriber2 = new MerloSubscriber("Ivanov");
+
+            eventObserver.NewsArticle += subscriber1.ItIsSecondSubscriber;
+            eventObserver.NewsArticle += subscriber2.ItIsSecondSubscriber;
+
+            Console.ReadKey();
+
+            Console.WriteLine("-- Отписка от сообщений --");
+            eventObserver.NewsArticle -= subscriber2.ItIsSecondSubscriber;
+
+            Console.ReadKey();
+        }
+    }
+}
